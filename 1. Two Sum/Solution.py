be7d2c8738c203b3a199ba9430c14c55929e31b5
shigeumchi <133833372+dkim3005@users.noreply.github.com>
2025-07-10 07:27:53 +0900

    Python Solution
          Submission Statistics:
          Question #: 1
          Language: Python
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 13.152 MB
          Memory percentile: 60.90

--- 1. Two Sum/Solution.py
+++ 1. Two Sum/Solution.py
@@ -5,10 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j] + nums[i] == target:
-                    return [i,j]
+        # Brute Force
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[j] + nums[i] == target:
+        #             return [i,j]
+        
+        # Hash Map
+        seen = {}
+        for i, n in enumerate(nums):
+            # print(i,n)
+            complement = target - n
+            if complement in seen:
+                return [seen[complement],i]
+            seen[n] = i
+            
+            
+
         
 
 
